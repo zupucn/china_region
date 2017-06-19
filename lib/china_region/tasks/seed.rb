79c992d689fdeaf2242fd7048e6be8ecbee56f27
Onizuka <onizuka1229@gmail.com>
2017-06-19 14:33:42 +0800

    Fix code style

--- lib/china_region/tasks/seed.rb
+++ lib/china_region/tasks/seed.rb
@@ -1,5 +1,5 @@
 namespace :china_region do
-  task :seed, [:batch_size] => [:environment]  do |t, args|
+  task :seed, [:batch_size] => [:environment]  do |_ , args|
     ChinaRegion::Region.init_db(args[:batch_size] || 500)
   end
 end
