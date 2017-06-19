37645f9d7af8e52938ecfa1ec644ff516d6bd156
Onizuka <onizuka1229@gmail.com>
2017-06-19 11:54:28 +0800

    Add task to rails

--- lib/china_region/tasks/seed.rb
+++ lib/china_region/tasks/seed.rb
@@ -1,5 +1,5 @@
 namespace :china_region do
-  task :seed do
-    ChinaRegion::Region.init_db
+  task :seed, [:batch_size] => [:environment]  do |t, args|
+    ChinaRegion::Region.init_db(args[:batch_size] || 500)
   end
 end
